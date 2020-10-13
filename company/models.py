4878269954a9bc396e709665af8af20697defe80
Kiran Johns <kiranjohns369@gmail.com>
2020-10-13 17:26:29 +0530

    job model

--- company/models.py
+++ company/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from user.models import Dev, Hr
 # Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.TextField(max_length=200)
+
+class Job(models.Model):
+    title = models.CharField(max_length=100)
+    des = models.TextField(max_length=500)
+    company = models.ForeignKey(Company,on_delete=models.CASCADE)
+    posted_by = models.OneToOneField(Hr, on_delete=models.CASCADE)
+    applicants = models.ManyToManyField(Dev,related_name="applied_jobs")
