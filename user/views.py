9f5aabeb800a7419f36478c2846df61ffd641a2b
Kiran Johns <kiranjohns369@gmail.com>
2020-11-07 11:50:29 +0530

    contact and about us

--- user/views.py
+++ user/views.py
@@ -159,3 +159,9 @@ def applied_jobs(request):
 
 def error404(request,exception):
     return render(request,'404.html',status=404)
+
+def about_us(request):
+    return render(request,'about.html')
+
+def contact_us(request):
+    return render(request,'contact-us.html')
